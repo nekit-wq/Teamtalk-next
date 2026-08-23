ede51d3e8cc93e5997d03ffc180ef4bb90ba5077
Ivankolp <ivankolp@users.noreply.github.com>
2026-08-23 15:57:24 +0300

    fix: add missing MenuItem imports in activities for Up navigation (v5.26.5)

--- src/main/java/org/nekit/ttproplus/gui/OnlineUsersActivity.java
+++ src/main/java/org/nekit/ttproplus/gui/OnlineUsersActivity.java
@@ -32,6 +32,7 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
--- src/main/java/org/nekit/ttproplus/gui/ServerStatsActivity.java
+++ src/main/java/org/nekit/ttproplus/gui/ServerStatsActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
